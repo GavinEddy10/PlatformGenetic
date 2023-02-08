313f82600778745d96a5099d3054f0418fc66201
gavin <geddy2023@damien-hs.edu>
2023-02-08 12:12:00 -0800

    Second Commit

--- main.py
+++ main.py
@@ -1,63 +1,133 @@
 import random
 import pygame
-from player import *
 
-# start the pygame engine
+
+from player import *
+from Player_ai import *
+
+
+#start the pygame engine
 pygame.init()
 
-# start the pygame font engine
+#start the pygame font engine
 pygame.font.init()
-myfont = pygame.font.SysFont('Comic Sans MS', 23)  # load a font for use
+myfont = pygame.font.SysFont('Comic Sans MS', 23) #load a font for use
 
-# start the sound engine
-pygame.mixer.init()
+#start the sound engine
+pygame.mixer.init();
 
-# game variables
+#game variables
 simOver = False
-p1 = Player()
+ai_players = []
 map1 = Map()
+camera_offset = (0,0)
+camera_pos = (500,500)
 
+#game independent variables (needed for every pygame)
+FPS = 60 #60 Frames Per Second for the game update cycle
+fpsClock = pygame.time.Clock() #used to lock the game to 60 FPS
+screen = pygame.display.set_mode((1280,720)) #initialize the game window
+world = pygame.Surface((3000,3000))
 
-# game independent variables (needed for every pygame)
-FPS = 60  # 60 Frames Per Second for the game update cycle
-fpsClock = pygame.time.Clock()  # used to lock the game to 60 FPS
-screen = pygame.display.set_mode((1280, 720))  # initialize the game window
-
+def create_ai_players():
+    for i in range(50):
+        ai_players.append(PlayerAI())
 
 def create_map_1():
-    map1.add(Platform(100, 600, 400, 30, (0, 255, 0)))
+    map1.add(Platform(0,690,2400,30,(0,255,0)))
+    map1.add(Platform(100,600,400,30,(0,255,0)))
+    map1.add(Platform(200,500,400,30,(0,255,0)))
+    map1.add(Platform(100,350,200,30,(0,255,0)))
+    map1.add(Platform(270,250,200,30,(0,255,0)))
+    map1.add(Platform(600,200,30,400,(0,255,0)))
+    map1.add(Platform(715,120,300,30,(0,255,0)))
+    map1.add(Platform(740,700,300,30,(0,255,0)))
+    map1.add_coin(Coin(600,650))
+    map1.add_coin(Coin(220,450))
+    map1.add_coin(Coin(730,80))
     map1.set_gravity(-4)
 
+def draw_mouse_coords():
+    textSurface = myfont.render(str(pygame.mouse.get_pos()), True, (255,255,255))
+    world.blit(textSurface, (50, 30))
+    #textSurface = myfont.render(str(ai_players[0].get_current_allele()), True, (255,255,255))
+    #world.blit(textSurface, (50, 70))
 
 def clear_screen():
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1280, 720))
+    pygame.draw.rect(world, (0,0,0), (0, 0, world.get_rect().width, world.get_rect().height))
 
+def update_camera():
+    global camera_pos
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        camera_pos = (camera_pos[0]+10, camera_pos[1])
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        camera_pos = (camera_pos[0]-10, camera_pos[1])
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        camera_pos = (camera_pos[0], camera_pos[1]-10)
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        camera_pos = (camera_pos[0], camera_pos[1]+10)
 
-# initalize all data before gameplay
+def sort_ai_by_score():
+    for i in range(len(ai_players)-1):
+        for j in range(len(ai_players)-2):
+            p1 = ai_players[j]
+            p2 = ai_players[j+1]
+            if p1.get_score() < p2.get_score():
+                temp = p1
+                ai_players[j] = p2
+                ai_players[j+1] = temp
+
+# no worky
+def kill_bottom_half():
+    global ai_players
+    newList = ai_players[0:int(len(ai_players)/2)]
+    ai_players.clear()
+    ai_players = newList
+
+# initialize all data before gameplay
 create_map_1()
-p1.setMap(map1)
+create_ai_players()
+for p in ai_players:
+    p.setMap(map1)
 
-
-
-# main while loop
+#main while loop
 while not simOver:
-    # loop through and empty the event queue, key presses
-    # buttons, clicks, etc.
-
+    #loop through and empty the event queue, key presses
+    #buttons, clicks, etc.
     for event in pygame.event.get():
-        # if the event is a click on the "X" close button
+        #if the event is a click on the "X" close button
         if event.type == pygame.QUIT:
             simOver = True
 
     # draw code
     clear_screen()
-    map1.draw(screen)
-    p1.draw(screen)
+    map1.draw(world)
+    for p in ai_players:
+        p.draw(world)
+    draw_mouse_coords()
 
-    p1.act()
+    # player update code
+    update_camera()
 
+    # if the first ai is done, they are all done, reset all of them
+    if ai_players[0].is_done():
+        print ("done")
+        for p in ai_players:
+            sort_ai_by_score()
+            kill_bottom_half()
+            p.reset()
 
-    # put all the graphics on the screen
-    # should be the LAST LINE of game code
+    for p in ai_players:
+        p.act()
+    x_offset = 0
+    y_offset = 0
+    x_offset = 640 - camera_pos[0]
+    y_offset = 350 - camera_pos[1]
+    camera_offset = (x_offset, y_offset)
+    #camera_pos = ((player_pos[0], player_pos[1] - 900))
+
+    #put all the graphics on the screen
+    #should be the LAST LINE of game code
+    screen.blit(world,camera_offset)
     pygame.display.flip()
-    fpsClock.tick(FPS)  # slow the loop down to 60 loops per second
+    fpsClock.tick(FPS) #slow the loop down to 60 loops per second
--- player.py
+++ player.py
@@ -1,25 +1,49 @@
 import pygame
 
-
 class Player:
 
     # empty constructor
-    def __init__(self):
+    def __init__(self, color=(255,0,0)):
         self.x = 400
         self.y = 400
         self.width = 30
         self.height = 30
-        self.color = (255, 0, 0)
+        self.color = color
         self.isJumping = False
         self.currentJumpVel = 40
         self.maxJumpVel = 40
+        self.speed = 10
+        self.coins = 0
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
+    def jump(self):
+        if not self.isJumping:
+            self.isJumping = True
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def move_left(self):
+        self.x += self.speed
+
+    def move_right(self):
+        self.x -= self.speed
+
     def handle_key_presses(self):
         if pygame.key.get_pressed()[pygame.K_SPACE] and not self.isJumping:
             self.isJumping = True
+        if pygame.key.get_pressed()[pygame.K_d]:
+            self.x += self.speed
+        if pygame.key.get_pressed()[pygame.K_a]:
+            self.x -= self.speed
 
     def act(self):
         oldx = self.x
@@ -28,11 +52,27 @@ class Player:
         self.handle_key_presses()
         self.handleJump()
         self.y -= self.map.get_gravity()
-        if self.is_map_collision():
-            self.x = oldx
+        if self.is_map_bottom_collision():
             self.y = oldy
             self.isJumping = False
             self.currentJumpVel = self.maxJumpVel
+        if self.is_map_right_collision():
+            self.x = oldx
+
+        self.check_coin_collision()
+
+        return (self.x, self.y)
+
+    def check_coin_collision(self):
+        coins = self.map.get_coins()
+        myHitBox = pygame.Rect(self.x, self.y, self.width, self.height)
+        for c in coins:
+            if myHitBox.colliderect(c.getCollisionRect()):
+                coins.remove(c)
+                self.coins += 1
+
+    def get_score(self):
+        return self.coins
 
     def handleJump(self):
         if self.isJumping:
@@ -41,6 +81,7 @@ class Player:
             # terminal velocity
             if self.currentJumpVel < -self.maxJumpVel:
                 self.currentJumpVel = -self.maxJumpVel
+
 
     def setMap(self, map):
         self.map = map
@@ -52,6 +93,30 @@ class Player:
             if myHitBox.colliderect(box):
                 return True
 
+        return False
+
+    def is_map_bottom_collision(self):
+        myHitBox = pygame.Rect(self.x, self.y, self.width, self.height)
+        myBottomY = myHitBox.y + myHitBox.height
+        mapHitBoxes = self.map.get_hit_box_list()
+        for box in mapHitBoxes:
+            if myHitBox.colliderect(box):
+                if myBottomY > box.y: # I am hitting and BELOW this platform
+                    return True;
+
+        return False
+
+    def is_map_right_collision(self):
+        myHitBox = pygame.Rect(self.x, self.y, self.width, self.height)
+        myRightX = myHitBox.x + myHitBox.width
+        mapHitBoxes = self.map.get_hit_box_list()
+        for box in mapHitBoxes:
+            if myHitBox.colliderect(box):
+                if myRightX > box.x: # I am hitting and to the RIGHT this platform
+                    return True;
+
+        return False
+
 
 class Platform:
 
@@ -62,19 +127,22 @@ class Platform:
         self.height = height
         self.color = color
 
-    # used for drawing and used for collision detection
+    #used for drawing and used for collision detection
     def getRect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.getRect())
-
+        pygame.draw.rect(screen, self.color, self.getRect());
 
 class Map:
 
     def __init__(self, gravity=-0.5):
         self.platforms = []
         self.gravity = gravity
+        self.coins = []
+
+    def add_coin(self, coin):
+        self.coins.append(coin)
 
     def get_gravity(self):
         return self.gravity
@@ -89,9 +157,29 @@ class Map:
         hitBoxes = []
         for p in self.platforms:
             hitBoxes.append(p.getRect())
+
         return hitBoxes
 
-    # precondition: platforms list is a homogeneous list of platform objects
+    def get_coins(self):
+        return self.coins
+
+    # Precondition: the platforms list is a homogeneous list of Platform objects
     def draw(self, screen):
         for p in self.platforms:
             p.draw(screen)
+        for c in self.coins:
+            c.draw(screen)
+
+class Coin:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 30
+        self.color = (255,255,0)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+
+    def getCollisionRect(self):
+        return pygame.Rect(self.x, self.y, self.size, self.size)
